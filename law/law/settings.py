aef7e63402bd7f742f7a63715a16c7672ac99356
jongmin park <showppark12@naver.com>
2020-01-03 12:42:32 +0900

    base.html jongmin

--- law/law/settings.py
+++ law/law/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'law.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['law/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
