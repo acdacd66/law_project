99aed2804615764639d88dfd752268bd8bb8b9be
jongmin park <showppark12@naver.com>
2020-01-09 20:36:08 +0900

    jongmin

--- law/law/urls.py
+++ law/law/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),   
     path('account/', include('account.urls')),
+    path('lawboard/', include('lawboard.urls')),
 ]
