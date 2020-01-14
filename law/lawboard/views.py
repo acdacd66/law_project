f91f32afd96aa4cd9b0963f8ae382dcedf2250d9
mins <cms4317@kookmin.ac.kr>
2020-01-14 16:15:19 +0900

    MeetingBoard

--- law/lawboard/views.py
+++ law/lawboard/views.py
@@ -39,6 +39,42 @@ def lawboardDelete(request, lb_id):
     delete_lb.delete()
     return redirect('lb_list')
 
+def meetingboardList(request):
+    meetingboards= MeetingBoard.objects.all()
+    return render(request, 'Meetingboard_list.html', {'meetingboards': meetingboards})
+
+def meetingboardNew(request):
+    return render(request, 'meetingboard_new.html')
+
+def meetingboardCreate(request):
+    new_mb = MeetingBoard()
+    new_mb.title = request.POST['title']
+    new_mb.pub_date = timezone.datetime.now()
+    new_mb.writer = request.user
+    new_mb.body = request.POST['body']
+    new_mb.save()
+
+    return redirect('mb_list')
+
+def meetingboardEdit(request, mb_id):
+    edit_mb = MeetingBoard.objects.get(id = mb_id)
+    return render(request,'meetingboard_edit.html',{'mb': edit_mb})
+
+def meetingboardUpdate(request, mb_id):
+    update_mb = MeetingBoard.objects.get(id = mb_id)
+    update_mb.title = request.POST['title']
+    update_mb.body = request.POST['body']
+    update_mb.save()
+    return redirect('mb_list')
+
+def meetingboardDetail(request, lb_id):
+    detail_mb = MeetingBoard.objects.get(id=mb_id)
+    return render(request, 'meetingboard_detail.html',{'lb':detail_mb})
+
+def meetingboardDelete(request, lb_id):
+    delete_mb = MeetingBoard.objects.get(id=mb_id)
+    delete_mb.delete()
+    return redirect('mb_list')
 
 
 
